c6db9a9c8ac82ade1d426c0bb5b41efca1e8e7d8
Victor Lliuya <alonso.victor38@gmail.com>
2022-06-06 07:39:06 -0500

    Se guarda api rest

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const app = express()
+//Llamada de paquetes
+const express = require("express");
+const mongoose = require("mongoose");
+const usuarios = require("./routes/usuarios");
+const productos = require("./routes/productos");
+require("dotenv").config();
 
-app.get('/hola', function (req, res) {
-  res.send('Hola Mundo')
-  console.log("Hola Mundo")
-})
+//Inicializando variables
+const app = express();
+const port = 9000;
 
-app.listen(3000)
+//Configuraciones
+app.use(express.json());
+app.use("/api", usuarios);
+
+//Rutas
+app.get('/', (req, res) => {
+  res.send("Bienvenido al API Rest de Usuarios");
+});
+
+//Test MongoDB
+mongoose.connect(process.env.mongodb_conexion)
+  .then(() => console.log("Conexión realizada a MongoDBAtlas"))
+  .catch((error) => console.log(error))
+
+//Servidor
+app.listen(9000, () => console.log("Servidor escuchando en el puerto", port));
